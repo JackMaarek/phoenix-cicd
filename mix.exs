86a2dc1b3d983478905a92143cf30f293ccde317
Florian Guilbaud <florian.guilbaud.work@gmail.com>
2020-02-11 19:44:14 +0200

    Missed to add coveralls in the dependances

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Phoenixcicd.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -43,7 +50,8 @@ defmodule Phoenixcicd.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.1"}
+      {:distillery, "~> 2.1"},
+      {:excoveralls, "~> 0.12.2", only: :test}
     ]
   end
 
